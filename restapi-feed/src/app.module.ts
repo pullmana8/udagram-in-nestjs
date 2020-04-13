157ee73ada80f18bb3c26476c0d79eadce2a26db
Antonette Caldwell <pullmana8@gmail.com>
2020-04-13 12:25:03 -0500

    Ran command "nest g service feeds"

--- restapi-feed/src/app.module.ts
+++ restapi-feed/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeedsService } from './feeds/feeds.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FeedsService],
 })
 export class AppModule {}
